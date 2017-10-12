613e9d644663e747608fa09010d9a7a1dccaf8ca
vahbuna <silver.estla@gmail.com>
2017-10-12 11:13:54 +0200

    Lesson 8 clustering assignments

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
+"""
     Skeleton code for k-means clustering mini-project.
 """
 
@@ -40,31 +40,40 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-### there's an outlier--remove it! 
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
+#find min max of features: exercised_stock_options and salary
+vals = [pers_dict["salary"] for key, pers_dict in data_dict.iteritems() \
+        if pers_dict["salary"]!= 'NaN']
+print max(vals), min(vals)
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+### the input features we want to use
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-poi  = "poi"
+feature_3 = "total_payments"
+poi = "poi"
+#features_list = [poi, feature_1, feature_2, feature_3]
 features_list = [poi, feature_1, feature_2]
-data = featureFormat(data_dict, features_list )
-poi, finance_features = targetFeatureSplit( data )
+data = featureFormat(data_dict, features_list)
+poi, finance_features = targetFeatureSplit(data)
 
 
 ### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
+#for f1, f2, _ in finance_features:
 for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2).fit(data)
+pred = kmeans.labels_
 
 
 
