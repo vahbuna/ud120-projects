6531c931dfef381e721dcc13a9e9b38f52dc5f71
vahbuna <silver.estla@gmail.com>
2017-10-19 15:56:46 +0200

    lesson 14 validation assignments

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -28,5 +28,18 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn import tree
 
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+#17
+print clf.score(features, labels)
+#18
+from sklearn.model_selection import train_test_split
 
+features_train, features_test, labels_train, labels_test = \
+        train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf_train_test = clf.fit(features_train, labels_train)
+
+print clf_train_test.score(features_test, labels_test)
